758a1f198ab98152379d00f4ff928d2a60b07b78
root <root@taumedia.ykoon.nl>
2012-11-27 13:42:40 +0100

    init

---
+++ Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace polygram\PHPCRBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/hello/{name}")
+     * @Template()
+     */
+    public function indexAction($name)
+    {
+        return array('name' => $name);
+    }
+}
---
+++ polygramPHPCRBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace polygram\PHPCRBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class polygramPHPCRBundle extends Bundle
+{
+}
