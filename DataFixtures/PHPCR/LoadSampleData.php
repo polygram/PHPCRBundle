8ba6ebfac35c9aabc69c0c953ab238bb3a777ef7
Hristo Salabashev <ichoman@gmail.com>
2012-11-27 16:38:32 +0100

    fixtures

---
+++ DataFixtures/PHPCR/LoadSampleData.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace polygram\PHPCRBundle\DataFixtures\PHPCR;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use PHPCR\Util\NodeHelper;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+use polygram\PHPCRBundle\Document\User;
+
+class LoadSimpleCmsData extends ContainerAware implements FixtureInterface
+{
+    public function load(ObjectManager $dm)
+    {
+        $session = $dm->getPhpcrSession();
+
+        $basepath = "/users";
+
+        if ($session->nodeExists($basepath)) {
+            $session->removeItem($basepath);
+        }
+
+        NodeHelper::createPath($session, $basepath);
+        $base = $dm->find(null, $basepath);
+
+        
+
+        $dm->flush();
+    }
+}
